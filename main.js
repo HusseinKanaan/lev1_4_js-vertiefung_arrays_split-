5bca8d0a46aa9f8d02a455d00085ca96e9c104a6
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-08 15:27:40 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+let meinText1 = 'Hallo Welt. Ich wünsche euch allen einen guten Arbeitstag.';
+let meinText2 = 'Wie gehts heute?';
+let meinText3 = 'Heute ist ein großer Tag für uns.';
+
+
+let split1;
+let split2;
+let split3;
+
+
+split1 = meinText1.split();
+split2 = meinText1.split("");
+split3 = meinText1.split(" ");
+
+split1 = meinText2.split();
+split2 = meinText2.split("");
+split3 = meinText2.split(" ");
+
+split1 = meinText3.split();
+split2 = meinText3.split("");
+split3 = meinText3.split(" ");
+
+
+console.log(meinText1)
+
+console.log(meinText2)
+
+console.log(meinText3)
